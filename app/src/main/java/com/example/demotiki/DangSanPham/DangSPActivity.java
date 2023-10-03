a0382212efafeef3b1faf4d849a161c88bca1ee2
zira2801 <duythanhcao456@gmail.com>
2023-10-04 00:12:56 +0700

    finished project

--- app/src/main/java/com/example/demotiki/DangSanPham/DangSPActivity.java
+++ app/src/main/java/com/example/demotiki/DangSanPham/DangSPActivity.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -18,6 +19,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,7 +32,7 @@ import com.example.demotiki.R;
 
 import java.util.ArrayList;
 
-public class DangSPActivity extends AppCompatActivity implements RecycleAdapter.CountImage{
+public class DangSPActivity extends AppCompatActivity implements RecycleAdapter.CountImage,RecycleAdapter.itemClickListener{
 
     String[] item = {"Điện thoại","Laptop","Trang điểm"};
 
@@ -44,11 +47,14 @@ public class DangSPActivity extends AppCompatActivity implements RecycleAdapter.
     private static final int PICK_IMAGE = 1;
     AutoCompleteTextView autoCompleteTextView;
     ArrayAdapter<String> adapter;
+    Button removeall;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_spactivity);
         UIinit();
+        totalanh.setVisibility(View.GONE);
+        removeall.setVisibility(View.GONE);
         autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.auto_item);
         adapter = new ArrayAdapter<>(this,R.layout.list_danhmucsp,item);
         autoCompleteTextView.setAdapter(adapter);
@@ -79,10 +85,21 @@ public class DangSPActivity extends AppCompatActivity implements RecycleAdapter.
                 startActivityForResult(Intent.createChooser(intent,"Select Picture"),PICK_IMAGE);
             }
         });
-        imageadapter = new RecycleAdapter(uris,DangSPActivity.this,this::clicked);
+        imageadapter = new RecycleAdapter(uris,DangSPActivity.this,this::clicked,this::itemClick);
         recyclerView.setLayoutManager(new GridLayoutManager(DangSPActivity.this,4));
         recyclerView.setAdapter(imageadapter);
 
+        //Xóa tất cả các ảnh
+        removeall.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onClick(View view) {
+                uris.clear();
+                imageadapter.notifyDataSetChanged();
+                totalanh.setVisibility(View.GONE);
+                removeall.setVisibility(View.GONE);
+            }
+        });
     }
 
     @SuppressLint({"SetTextI18n", "NotifyDataSetChanged"})
@@ -105,8 +122,10 @@ public class DangSPActivity extends AppCompatActivity implements RecycleAdapter.
                 imageadapter.notifyDataSetChanged();
                 if(uris.size() == 0){
                     totalanh.setVisibility(View.GONE);
+                    removeall.setVisibility(View.GONE);
                 }
                 else {
+                    removeall.setVisibility(View.VISIBLE);
                     totalanh.setVisibility(View.VISIBLE);
                     totalanh.setText("Số lượng ảnh: " + uris.size());
                 }
@@ -116,6 +135,9 @@ public class DangSPActivity extends AppCompatActivity implements RecycleAdapter.
                 Uri imageuri = data.getData();
                 uris.add(imageuri);
             }
+
+            removeall.setVisibility(View.VISIBLE);
+            totalanh.setVisibility(View.VISIBLE);
             imageadapter.notifyDataSetChanged();
             totalanh.setText("Số lượng ảnh: " + uris.size());
         }
@@ -128,17 +150,40 @@ public class DangSPActivity extends AppCompatActivity implements RecycleAdapter.
         totalanh = findViewById(R.id.totalimage);
         recyclerView = (RecyclerView) findViewById(R.id.recycleview_chonanh);
         chonanh = findViewById(R.id.chonhinhanh);
+        removeall = (Button) findViewById(R.id.btn_removeall);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void clicked(int getSize) {
         if(uris.size() == 0){
             totalanh.setVisibility(View.GONE);
+            removeall.setVisibility(View.GONE);
         }
         else {
+            removeall.setVisibility(View.VISIBLE);
             totalanh.setVisibility(View.VISIBLE);
             totalanh.setText("Số lượng ảnh: " + uris.size());
         }
     }
 
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void itemClick(int position) {
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_zoom);
+        TextView textimg = dialog.findViewById(R.id.text_image);
+        ImageView img = dialog.findViewById(R.id.image_dialog);
+        Button button = dialog.findViewById(R.id.close_dialogimg);
+
+        textimg.setText("Ảnh "+position);
+        img.setImageURI(uris.get(position));
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
 }
--- app/src/main/java/com/example/demotiki/DangSanPham/RecycleAdapter.java
+++ app/src/main/java/com/example/demotiki/DangSanPham/RecycleAdapter.java
@@ -18,19 +18,21 @@ import java.util.ArrayList;
 public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.RecyclerViewHolder> {
 
     private final ArrayList<Uri> uris;
-    private Context mcontext;
+    private final Context mcontext;
     CountImage countImage;
-    public RecycleAdapter(ArrayList<Uri> arrayList, Context context,CountImage countImage){
+    private final itemClickListener itemClickListener1;
+    public RecycleAdapter(ArrayList<Uri> arrayList, Context context,CountImage countImage,itemClickListener itemClickListener){
         this.uris = arrayList;
         this.mcontext = context;
         this.countImage = countImage;
+        this.itemClickListener1 = itemClickListener;
     }
     @NonNull
     @Override
     public RecycleAdapter.RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.custom_image,parent,false);
-        return new RecyclerViewHolder(view,countImage);
+        return new RecyclerViewHolder(view,countImage,itemClickListener1);
     }
 
     @Override
@@ -52,19 +54,36 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.Recycler
         return uris.size();
     }
 
-    public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+    public class RecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         ImageView imageView,delete;
         CountImage countImage;
-        public RecyclerViewHolder(@NonNull View itemView, CountImage countImage) {
+        itemClickListener itemClickListener;
+        public RecyclerViewHolder(@NonNull View itemView, CountImage countImage,itemClickListener itemClickListener) {
             super(itemView);
             this.countImage = countImage;
             imageView = itemView.findViewById(R.id.image_select);
             delete = itemView.findViewById(R.id.delete_image);
+            this.itemClickListener = itemClickListener;
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if(itemClickListener != null){
+                itemClickListener.itemClick(getAdapterPosition());
+            }
         }
     }
 
+    //Interface số lượng ảnh
     public interface CountImage{
         void clicked(int getSize);
+    }
+
+    //Interface zoom ảnh
+    public interface itemClickListener{
+        void itemClick(int position);
     }
 }
