e6f49f37eacd8fab286f52a111db5ee821c6dbd3
zira2801 <duythanhcao456@gmail.com>
2023-10-03 20:04:06 +0700

    finished project

--- app/src/main/java/com/example/demotiki/DangSanPham/DangSPActivity.java
+++ app/src/main/java/com/example/demotiki/DangSanPham/DangSPActivity.java
@@ -1,37 +1,54 @@
 package com.example.demotiki.DangSanPham;
-
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
 
 import com.example.demotiki.R;
 
+
+
 import java.util.ArrayList;
 
-public class DangSPActivity extends AppCompatActivity {
+public class DangSPActivity extends AppCompatActivity implements RecycleAdapter.CountImage{
 
     String[] item = {"Điện thoại","Laptop","Trang điểm"};
 
     RecyclerView recyclerView;
     RelativeLayout chonanh;
 
+    TextView totalanh;
+    RecycleAdapter imageadapter;
     ArrayList<Uri> uris = new ArrayList<>();
 
+    private static final int Read_Permission = 101;
+    private static final int PICK_IMAGE = 1;
     AutoCompleteTextView autoCompleteTextView;
     ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_spactivity);
-
+        UIinit();
         autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.auto_item);
         adapter = new ArrayAdapter<>(this,R.layout.list_danhmucsp,item);
         autoCompleteTextView.setAdapter(adapter);
@@ -42,5 +59,86 @@ public class DangSPActivity extends AppCompatActivity {
                 Toast.makeText(DangSPActivity.this,"Item: "+item,Toast.LENGTH_SHORT).show();
             }
         });
+
+        if(ContextCompat.checkSelfPermission(DangSPActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(DangSPActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},Read_Permission);
+        }
+        chonanh.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ObsoleteSdkInt")
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+                    intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
+                }
+                //intent.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"),PICK_IMAGE);
+            }
+        });
+        imageadapter = new RecycleAdapter(uris,DangSPActivity.this,this::clicked);
+        recyclerView.setLayoutManager(new GridLayoutManager(DangSPActivity.this,4));
+        recyclerView.setAdapter(imageadapter);
+
     }
+
+    @SuppressLint({"SetTextI18n", "NotifyDataSetChanged"})
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK && null != data){
+            if(data.getClipData() != null){
+                int x = data.getClipData().getItemCount();
+                for (int i =0;i<x;i++){
+                    if(uris.size() < 3){
+                        Uri image = data.getClipData().getItemAt(i).getUri();
+                        uris.add(image);
+                    }
+                    else{
+                        Toast.makeText(this,"Bạn chỉ lấy được giới hạn 3 ảnh",Toast.LENGTH_SHORT).show();
+                    }
+                }
+                imageadapter.notifyDataSetChanged();
+                if(uris.size() == 0){
+                    totalanh.setVisibility(View.GONE);
+                }
+                else {
+                    totalanh.setVisibility(View.VISIBLE);
+                    totalanh.setText("Số lượng ảnh: " + uris.size());
+                }
+            }
+            else{
+                //Cai nay là danh cho chọn 1 ảnh
+                Uri imageuri = data.getData();
+                uris.add(imageuri);
+            }
+            imageadapter.notifyDataSetChanged();
+            totalanh.setText("Số lượng ảnh: " + uris.size());
+        }
+        else{
+            Toast.makeText(this,"Bạn chưa chọn ảnh !",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void UIinit(){
+        totalanh = findViewById(R.id.totalimage);
+        recyclerView = (RecyclerView) findViewById(R.id.recycleview_chonanh);
+        chonanh = findViewById(R.id.chonhinhanh);
+    }
+
+    @Override
+    public void clicked(int getSize) {
+        if(uris.size() == 0){
+            totalanh.setVisibility(View.GONE);
+        }
+        else {
+            totalanh.setVisibility(View.VISIBLE);
+            totalanh.setText("Số lượng ảnh: " + uris.size());
+        }
+    }
+
 }
--- app/src/main/java/com/example/demotiki/DangSanPham/RecycleAdapter.java
+++ app/src/main/java/com/example/demotiki/DangSanPham/RecycleAdapter.java
@@ -1,31 +1,70 @@
 package com.example.demotiki.DangSanPham;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.demotiki.R;
+
+import java.util.ArrayList;
+
 public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.RecyclerViewHolder> {
 
+    private final ArrayList<Uri> uris;
+    private Context mcontext;
+    CountImage countImage;
+    public RecycleAdapter(ArrayList<Uri> arrayList, Context context,CountImage countImage){
+        this.uris = arrayList;
+        this.mcontext = context;
+        this.countImage = countImage;
+    }
     @NonNull
     @Override
     public RecycleAdapter.RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.custom_image,parent,false);
+        return new RecyclerViewHolder(view,countImage);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull RecyclerViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        holder.imageView.setImageURI(uris.get(position));
+        holder.delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uris.remove(uris.get(position));
+                notifyItemRemoved(position);
+                notifyItemRangeChanged(position,getItemCount());
+                countImage.clicked(uris.size());
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return uris.size();
     }
 
     public class RecyclerViewHolder extends RecyclerView.ViewHolder {
-        public RecyclerViewHolder(@NonNull View itemView) {
+
+        ImageView imageView,delete;
+        CountImage countImage;
+        public RecyclerViewHolder(@NonNull View itemView, CountImage countImage) {
             super(itemView);
+            this.countImage = countImage;
+            imageView = itemView.findViewById(R.id.image_select);
+            delete = itemView.findViewById(R.id.delete_image);
         }
+    }
+
+    public interface CountImage{
+        void clicked(int getSize);
     }
 }
